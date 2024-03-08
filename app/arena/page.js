ea7f534859a5a5741cbf5a9b1f4914e201733b6e
Tanay-277 <tanayshinde081@gmail.com>
2024-03-08 20:13:39 +0530

    new file:   app/arena/arena.css 	modified:   app/arena/page.js 	modified:   app/globals.css

--- app/arena/page.js
+++ app/arena/page.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import "./arena.css";
 
 const page = () => {
   return (
-    <div>page</div>
-  )
-}
+    <div className="arena flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold text-center mb-4">
+        What is the capital of France?
+      </h1>
+      <p className="text-center text-gray-500">Hint: The city of love</p>
+      <div className="flex flex-col items-center justify-center gap-4 mt-4">
+        <input
+          type="text"
+          className="px-4 py-2 border-[0.1px] border-[#ffffff12] rounded-lg"
+        />
+        <button className="px-4 py-2 rounded-lg border-[0.1px] border-[#ffffff12]">
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default page
+export default page;
