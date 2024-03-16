237df3cf134c4b45b6113cc94d720d348827f172
Tanay-277 <tanayshinde081@gmail.com>
2024-03-16 16:28:25 +0530

    Add new dependencies for three.js and spline tools

---
+++ app/bye/page.js
@@ -0,0 +1,10 @@
+"use client";
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <>
+      <Spline scene="https://prod.spline.design/r2fxZA21HvoZdHtw/scene.splinecode" />
+    </>
+  );
+}
