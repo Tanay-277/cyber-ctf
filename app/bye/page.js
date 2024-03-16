3defd1dd70c8dc029162c97f48b50bc697af4bfb
Tanay-277 <tanayshinde081@gmail.com>
2024-03-16 16:56:09 +0530

    modified:   app/bye/page.js

--- app/bye/page.js
+++ app/bye/page.js
@@ -1,11 +1,15 @@
 "use client";
 import Spline from "@splinetool/react-spline";
-import Image from "next/image";
 
 export default function App() {
   return (
     <>
       <Spline scene="https://prod.spline.design/r2fxZA21HvoZdHtw/scene.splinecode" />
+      <div className="absolute top-0 left-1/3 h-10 w-full ">
+        <marquee behavior="scroll" direction="up">
+          Congratulations! <br/> You have successfully completed the test.<br/>You are a Crusader!
+        </marquee>
+      </div>
     </>
   );
 }
